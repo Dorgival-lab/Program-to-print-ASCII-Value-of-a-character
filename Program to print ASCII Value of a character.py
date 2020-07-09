c1a15dddf376f16333e1a652ad79f2b0d93d4051
Dorgival Fernando <59142443+Dorgival-lab@users.noreply.github.com>
2020-07-09 20:08:02 -0300

    Create Program to print ASCII Value of a character.py
    Resultado:
    
    O valor ASCII de g é 103

---
+++ Program to print ASCII Value of a character.py
@@ -0,0 +1,9 @@
+# Programa Python para imprimir
+# Valor ASCII de Caractere
+  
+# Em c, podemos atribuir diferentes
+# caracteres dos quais queremos valor ASCII 
+  
+c = 'g'
+# imprime o valor ASCII do caractere atribuído em c
+print("The ASCII value of '" + c + "' is", ord(c)) 
